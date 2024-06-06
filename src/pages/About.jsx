97c383a36cacb907d25a03a889e78de7872c74b2
akong8 <allison.kong@hotmail.com>
2024-06-05 22:49:58 -0700

    started the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,14 @@ const About = () => {
         <div>
             <h1>About/User Manual</h1>
             {/* Add information about the app and how to use it */}
+            <p>Welcome to StudySpotter! UBC's hottest study spot finder. StudySpotter is an app that facilitates
+                the location and reservation of study spots on campus. Users will be able to view a map, check out amenities, 
+                and reserve spaces that will fit their needs.
+
+                To get started, select any waypoint on the map. This will tell you basic information about that point.
+                Upon selection, a pop-up will appear and display all amenities available at that location along with
+                the option to book private rooms via a link.
+            </p>
         </div>
     );
 };
