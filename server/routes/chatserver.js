98ee206be0dee4170e5a2a1ff2264f90694cc20d
Gurman <gurmandeep.bal7@gmail.com>
2024-06-29 13:05:14 -0700

    made each chatbox independant

--- server/routes/chatserver.js
+++ server/routes/chatserver.js
@@ -7,25 +7,40 @@ const router = express.Router();
 const server = http.createServer(router);
 const io = socketIo(server);
 
-let messages = [];
+let messages = {};
 const welcomeMessage = { text: 'Welcome to the chat!', timestamp: new Date().toISOString() };
 
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    //socket.emit('message', welcomeMessage);
+    socket.on('join', (pinId) => {
+        socket.join(pinId);
 
-    // Send the existing messages to the new client
-    //socket.emit('init', messages);
+        if (!messages[pinId]) {
+            messages[pinId] = [];
+            messages[pinId].push(welcomeMessage);
+        }
 
-    // socket.emit('message', welcomeMessage);
+        // Send the existing messages for the pin to the new client
+        socket.emit('init', messages[pinId]);
+    });
     
 
     // Listen for new messages from clients
     socket.on('message', (message) => {
+
+        const { pinId } = message;
+
+        if (!messages[pinId]) {
+            messages[pinId] = [];
+        }
         
-        messages.push(message);
+        messages[pinId].push(message);
         io.emit('message', message); // Broadcast new message to all clients
+    });
+
+    socket.on('leave', (pinId) => {
+        socket.leave(pinId);
     });
 
     socket.on('disconnect', () => {
