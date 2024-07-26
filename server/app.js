fee65ba49b865ac82a1aff41fa30f85e885b3a59
Gurman <gurman04@student.ubc.ca>
2024-07-25 18:10:33 -0700

    revert app.js to old code

--- server/app.js
+++ server/app.js
@@ -1,3 +1,39 @@
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var chatRouter = require('./routes/chatserver');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// // app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'client/dist')));
+
+
+// app.use('/', indexRouter);
+// app.use('/chat', chatRouter);
+// app.use('/users', usersRouter);
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+// });
+
+// // Set the port number
+// var port = process.env.PORT || 3000;
+
+// // Start the server
+// app.listen(port, function() {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+// module.exports = app;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -13,25 +49,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/chat', chatRouter);
 app.use('/users', usersRouter);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-});
-
-// Set the port number
-var port = process.env.PORT || 3000;
-
-// Start the server
-app.listen(port, function() {
-    console.log(`Server is running on port ${port}`);
-});
 
 module.exports = app;
 
