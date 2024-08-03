496c1701425f45ae2fe5bad3455a1d86fe1f5174
akong8 <allison.kong@hotmail.com>
2024-08-03 15:44:41 -0700

    styling

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import '../App.css';
 import GoogleAuth from "../components/GoogleAuth.jsx";
 import ProfileComponent from "../components/ProfileComponent.jsx";
-import ubc from '../assets/ubc-official-logo.png';
 
 const Profile = () => {
   const session = useSelector((state) => state.session.sessionID);
+  const profileContainerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.1 });
+
+    if (profileContainerRef.current) {
+      observer.observe(profileContainerRef.current);
+    }
+
+    return () => {
+      if (profileContainerRef.current) {
+        observer.unobserve(profileContainerRef.current);
+      }
+    };
+  }, []);
 
   return (
-      <>
-        <h1>Welcome to UBC StudySpotter!</h1>
-        <div className="profile-container">
-          {!session && <h3 className="profile-header">Login with your Google Account to get started!</h3>}
-          <img className="ubc" src={ubc} alt="ubc" style={{ width: 'auto', height: '200px' }} />
-          <br />
-          <br />
-          <ProfileComponent />
-          <GoogleAuth />
-        </div>
-      </>
+    <>
+      <h1>Welcome to UBC StudySpotter!</h1>
+      <div className="profile-container fade-in" ref={profileContainerRef}>
+        {!session && <h3 className="profile-header">Login with your Google Account to get started!</h3>}
+        <br />
+        <br />
+        <ProfileComponent />
+        <GoogleAuth />
+      </div>
+    </>
   );
 };
 
 export default Profile;
+
