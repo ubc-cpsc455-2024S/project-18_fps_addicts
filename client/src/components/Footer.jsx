8e08f0b21ea6571fcff678ea71c2df604ee224f9
Gurman <gurman04@student.ubc.ca>
2024-08-04 18:02:20 -0700

    added a contact us old fashion way

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 const Footer = () => {
     return (
         <footer className="copyright-footer">
-            <p>&copy; {new Date().getFullYear()} StudySpotter. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} StudySpotter. All rights reserved. | Contact Use @ studyspotterubc@gmail.com</p>
         </footer>
     );
 };
